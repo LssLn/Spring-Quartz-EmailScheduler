ac4605761f9f0fc056b5e69c4781b94cc3fba22c
Alessio Leone <36887074+LssLn@users.noreply.github.com>
2022-04-27 20:36:07 +0200

    Add files via upload

---
+++ src/main/java/com/example/emailscheduler/payload/EmailRequest.java
@@ -0,0 +1,70 @@
+package com.example.emailscheduler.payload;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class EmailRequest { 
+	@Email //https://stackoverflow.com/questions/13649015/error-in-java-import-statement-the-import-javax-validation-constraints-notnull
+	@NotEmpty
+	private String email;
+	
+	@NotEmpty
+	private String subject;
+	
+	@NotEmpty
+	private String body;
+	
+	@NotEmpty
+	private LocalDateTime dateTime;
+	
+	@NotNull
+	private ZoneId timeZone;
+
+	
+	//getters setters
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public LocalDateTime getDateTime() {
+		return dateTime;
+	}
+
+	public void setDateTime(LocalDateTime dateTime) {
+		this.dateTime = dateTime;
+	}
+
+	public ZoneId getTimeZone() {
+		return timeZone;
+	}
+
+	public void setTimeZone(ZoneId timeZone) {
+		this.timeZone = timeZone;
+	}
+	
+	
+}
---
+++ src/main/java/com/example/emailscheduler/payload/EmailResponse.java
@@ -0,0 +1,5 @@
+package com.example.emailscheduler.payload;
+
+public class EmailResponse {
+
+}
